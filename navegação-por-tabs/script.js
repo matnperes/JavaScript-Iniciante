fe567ddee8101a21c280a8371c14420960959587
Matheus Peres <nobre_matheus@outlook.com>
2021-11-08 19:50:47 -0300

    scroll animação

--- navegação-por-tabs/script.js
+++ navegação-por-tabs/script.js
@@ -56,3 +56,24 @@ linksInternos.forEach((link) => {
 }
 
 initScroll();
+
+function initAnimaScroll(){
+const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight *0.6;
+
+function animaScroll(){
+  sections.forEach((section) =>{
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    if(isSectionVisible){
+      section.classList.add('ativo');
+    }
+  })
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+}
+
+initAnimaScroll();
