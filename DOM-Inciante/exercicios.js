59b1b83ff100786dcd33b2ec60048480ce4fdb00
Matheus Peres <nobre_matheus@outlook.com>
2021-11-18 21:14:44 -0300

    finalizando parte iniciante

--- DOM-Inciante/exercicios.js
+++ DOM-Inciante/exercicios.js
@@ -7,7 +7,7 @@ const ativo = document.querySelector(".ativo")
 console.log(ativo)
 
 const ativo1 = document.querySelectorAll(".ativo")
-console.log(ativo1[1])
+console.log(ativo1[0])
 
 // Retorne a linguagem do navegador
 const linguagem = window.navigator.language
